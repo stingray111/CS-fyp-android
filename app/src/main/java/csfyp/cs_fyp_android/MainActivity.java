1bc28644a94fb90ebd454b22cd0a9e4553857a4a
Ray LUK <pingshan1013@hotmail.com>
2016-10-13 10:32:36 +0800

    Merged Event Page

--- app/src/main/java/csfyp/cs_fyp_android/MainActivity.java
+++ app/src/main/java/csfyp/cs_fyp_android/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 
+import csfyp.cs_fyp_android.event.FrgEvent;
 import csfyp.cs_fyp_android.home.FrgHome;
 import csfyp.cs_fyp_android.profile.FrgProfile;
 
@@ -15,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.replace(R.id.parent_fragment_container, FrgProfile.newInstance());
+        ft.replace(R.id.parent_fragment_container, FrgEvent.newInstance());
         ft.commit();
     }
 }
