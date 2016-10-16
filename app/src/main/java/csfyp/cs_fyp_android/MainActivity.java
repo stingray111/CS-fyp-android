6192cb7ef31586980c96ab32da9f868baa7b7233
Ray LUK <pingshan1013@hotmail.com>
2016-10-16 21:36:49 +0800

    Completed event page map

--- app/src/main/java/csfyp/cs_fyp_android/MainActivity.java
+++ app/src/main/java/csfyp/cs_fyp_android/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 
 import csfyp.cs_fyp_android.databinding.ActivityMainBinding;
+import csfyp.cs_fyp_android.event.FrgEvent;
 import csfyp.cs_fyp_android.home.FrgHome;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.replace(R.id.parent_fragment_container, FrgHome.newInstance());
+        ft.replace(R.id.parent_fragment_container, FrgEvent.newInstance());
         ft.commit();
     }
 }
--- app/src/main/java/csfyp/cs_fyp_android/event/FrgEvent.java
+++ app/src/main/java/csfyp/cs_fyp_android/event/FrgEvent.java
@@ -1,6 +1,7 @@
 package csfyp.cs_fyp_android.event;
 
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -10,17 +11,31 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import csfyp.cs_fyp_android.CustomFragment;
 import csfyp.cs_fyp_android.R;
 
-/**
- * Created by ray on 13/10/2016.
- */
 
-public class FrgEvent extends Fragment {
+public class FrgEvent extends CustomFragment implements OnMapReadyCallback {
     public FrgEvent() {
         super();
     }
     private Toolbar mToolBar;
+    private GoogleMap mGoogleMap;
+    private GoogleApiClient client;
+    private MapView mMapView;
 
     public static FrgEvent newInstance() {
         
@@ -30,6 +45,13 @@ public class FrgEvent extends Fragment {
         fragment.setArguments(args);
         return fragment;
     }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        client = new GoogleApiClient.Builder(getContext()).addApi(AppIndex.API).build();
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
@@ -46,8 +68,93 @@ public class FrgEvent extends Fragment {
                 //TODO
             }
         });
+
+        mMapView = (MapView) v.findViewById(R.id.map);
+        mMapView.onCreate(null);
+        mMapView.getMapAsync(this);
+
+
         return v;
     }
 
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mGoogleMap  = googleMap;
+        mGoogleMap.getUiSettings().setZoomControlsEnabled(false);
+        mGoogleMap.getUiSettings().setMyLocationButtonEnabled(false);
+        mGoogleMap.getUiSettings().setMapToolbarEnabled(false);
+        //TODO: disable zoom by gesture
+
+        mGoogleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(22.25, 114.1667))
+                .title("Event 1")
+                .snippet("Custom")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_map_marker)));
+        mGoogleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(22.28, 114.1679))
+                .title("Event 2")
+                .snippet("Custom2")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_map_marker)));
+        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(22.25, 114.1667), 12.0f));
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
+    }
+
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
 }
+
