a40ed1e84045898fd5c8f85d148ad9f62fa0424a
ken31ee <tungpakyin04@outlook.com>
2016-10-20 14:48:54 +0800

    Fix

--- app/src/main/java/csfyp/cs_fyp_android/home/AdtHome.java
+++ app/src/main/java/csfyp/cs_fyp_android/home/AdtHome.java
@@ -1,14 +1,16 @@
 package csfyp.cs_fyp_android.home;
 
 import android.app.Activity;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import csfyp.cs_fyp_android.R;
+import csfyp.cs_fyp_android.event.FrgEvent;
 
 public class AdtHome extends RecyclerView.Adapter<AdtHome.ViewHolder> {
 
@@ -23,7 +25,7 @@ public class AdtHome extends RecyclerView.Adapter<AdtHome.ViewHolder> {
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.home_item_event, parent, false);
-        ViewHolder vh = new ViewHolder(v,((Activity) parent.getContext()).getFragmentManager());
+        ViewHolder vh = new ViewHolder(v,((FragmentActivity) parent.getContext()).getSupportFragmentManager());
         act = (Activity) parent.getContext();
         return vh;
     }
@@ -44,7 +46,7 @@ public class AdtHome extends RecyclerView.Adapter<AdtHome.ViewHolder> {
             // TODO: 20/10/2016 put switch fragment to
             FragmentTransaction ft = manager.beginTransaction();
             ft.setCustomAnimations(R.anim.frg_slide_top_enter, R.anim.frg_slide_bottom_exit, R.anim.frg_slide_bottom_enter, R.anim.frg_slide_top_exit)
-                    .replace(R.id.parent_fragment_container, fragment)
+                    .replace(R.id.parent_fragment_container, FrgEvent.newInstance())
                     .addToBackStack(null)
                     .commit();
 
