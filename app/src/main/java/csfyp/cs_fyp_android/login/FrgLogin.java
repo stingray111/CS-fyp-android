72d0d2cdac66c2cac9237006b33e5756b79681d8
Ray LUK <pingshan1013@hotmail.com>
2016-11-10 14:35:29 +0800

    Added Username, minor change in login, login validation

--- app/src/main/java/csfyp/cs_fyp_android/login/FrgLogin.java
+++ app/src/main/java/csfyp/cs_fyp_android/login/FrgLogin.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -12,7 +14,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.mobsandgeeks.saripaar.ValidationError;
+import com.mobsandgeeks.saripaar.Validator;
+import com.mobsandgeeks.saripaar.annotation.NotEmpty;
+import com.mobsandgeeks.saripaar.annotation.Password;
+
+import java.util.List;
 import java.util.UUID;
 
 import csfyp.cs_fyp_android.CustomFragment;
@@ -21,11 +31,12 @@ import csfyp.cs_fyp_android.home.FrgHome;
 import csfyp.cs_fyp_android.lib.HTTP;
 import csfyp.cs_fyp_android.model.Login;
 import csfyp.cs_fyp_android.model.LoginRespond;
+import csfyp.cs_fyp_android.register.FrgRegister;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class FrgLogin extends CustomFragment{
+public class FrgLogin extends CustomFragment implements Validator.ValidationListener{
     public FrgLogin(){
         super();
     }
@@ -33,8 +44,12 @@ public class FrgLogin extends CustomFragment{
     public final static String TAG = "login";
     private Toolbar mToolBar;
     private Button mLoginBtn;
+    @NotEmpty
     private EditText mInputEmailOrUsername;
+    @Password(min=8,scheme = Password.Scheme.ALPHA_NUMERIC)
     private EditText mInputPassword;
+    private TextView mRegisterBtn;
+    private Validator mValidator;
 
     public static FrgLogin newInstance() {
 
@@ -45,6 +60,9 @@ public class FrgLogin extends CustomFragment{
         return fragment;
     }
 
+    public void switchFragment(Fragment to) {
+        super.switchFragment(this, to);
+    }
 
     @Nullable
     @Override
@@ -55,65 +73,89 @@ public class FrgLogin extends CustomFragment{
         mToolBar.setTitle("Login");
         AppCompatActivity parentActivity = (AppCompatActivity) getActivity();
         parentActivity.setSupportActionBar(mToolBar);
-        mToolBar.setNavigationIcon(R.drawable.ic_previous_page);
-        mToolBar.setNavigationOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                onBack(TAG);
-            }
-        });
 
         mLoginBtn = (Button) v.findViewById(R.id.loginBtn);
         mInputEmailOrUsername = (EditText) v.findViewById(R.id.inputEmailOrUsername);
+
+        mValidator = new Validator(this);
+        mValidator.setValidationListener(this);
         mInputPassword = (EditText) v.findViewById(R.id.inputPassword);
-
-        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+        mRegisterBtn = (TextView) v.findViewById(R.id.registerBtn);
+        mRegisterBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                String strEmailOrUsername = mInputEmailOrUsername.getText().toString();
-                String strPassword = mInputPassword.getText().toString();
+            public void onClick(View v) {
+                switchFragment(FrgRegister.newInstance());
+            }
 
-                if (!strEmailOrUsername.matches("") && !strPassword.matches("")) {
-                    Log.i(TAG, strEmailOrUsername);
-                    Log.i(TAG, strPassword);
-
-                    String uuidInString = UUID.randomUUID().toString();
-
-                    HTTP httpService = HTTP.retrofit.create(HTTP.class);
-                    Login login = new Login(strEmailOrUsername, strPassword, uuidInString);
-                    Call<LoginRespond> call = httpService.login(login);
-
-                    call.enqueue(new Callback<LoginRespond>() {
-                        @Override
-                        public void onResponse(Call<LoginRespond> call, Response<LoginRespond> response) {
-                            if(response.isSuccessful()){
-                                if(response.body().isSuccessful()) {
-
-                                    // save token to cache
-                                    SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
-                                    SharedPreferences.Editor editor = sharedPref.edit();
-                                    editor.putString("userToken", response.body().getToken());
-                                    editor.commit();
-
-                                    replaceFragment(FrgHome.newInstance());
-
-                                } else {
-                                    // TODO: 6/11/2016 print error msg to user
-                                    Log.i(TAG, response.body().getErrorMsg());
-                                }
-                            }
-                        }
-
-                        @Override
-                        public void onFailure(Call<LoginRespond> call, Throwable t) {
-
-                        }
-                    });
-                }
+        });
+        mLoginBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                mValidator.validate();
             }
         });
-
         return v;
+    }
+
+    @Override
+    public void onValidationSucceeded() {
+            String strEmailOrUsername = mInputEmailOrUsername.getText().toString();
+            String strPassword = mInputPassword.getText().toString();
+
+            if (!strEmailOrUsername.matches("") && !strPassword.matches("")) {
+                Log.i(TAG, strEmailOrUsername);
+                Log.i(TAG, strPassword);
+
+                String uuidInString = UUID.randomUUID().toString();
+
+                HTTP httpService = HTTP.retrofit.create(HTTP.class);
+                Login login = new Login(strEmailOrUsername, strPassword, uuidInString);
+                Call<LoginRespond> call = httpService.login(login);
+
+                call.enqueue(new Callback<LoginRespond>() {
+                    @Override
+                    public void onResponse(Call<LoginRespond> call, Response<LoginRespond> response) {
+                        if (response.isSuccessful()) {
+                            if (response.body().isSuccessful()) {
+
+                                // save token to cache
+                                SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
+                                SharedPreferences.Editor editor = sharedPref.edit();
+                                editor.putString("userToken", response.body().getToken());
+                                editor.commit();
+
+                                replaceFragment(FrgHome.newInstance());
+
+                            } else {
+                                // TODO: 6/11/2016 print error msg to user
+                                Log.i(TAG, response.body().getErrorMsg());
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<LoginRespond> call, Throwable t) {
+
+                    }
+                });
+            }
+    }
+
+    @Override
+    public void onValidationFailed(List<ValidationError> errors) {
+        AppCompatActivity parentActivity = (AppCompatActivity) getActivity();
+        for (ValidationError error : errors) {
+            View view = error.getView();
+            String message = error.getCollatedErrorMessage(parentActivity);
+
+            // Display error messages ;)
+            if (view instanceof EditText) {
+                ((EditText) view).setError(message);
+            } else {
+                Toast.makeText(parentActivity,message,Toast.LENGTH_LONG).show();
+            }
+        }
+
     }
 }
 
