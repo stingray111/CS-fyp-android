12160296d8f2f06f95e1864e2750860cbce09e30
ken31ee <tungpakyin04@outlook.com>
2017-01-03 22:18:41 +0800

    Refactor Home to use CustomMapFragment

--- app/src/main/java/csfyp/cs_fyp_android/home/FrgHome.java
+++ app/src/main/java/csfyp/cs_fyp_android/home/FrgHome.java
@@ -1,21 +1,12 @@
 package csfyp.cs_fyp_android.home;
 
-import android.Manifest;
-import android.content.Intent;
-import android.content.IntentSender;
-import android.content.pm.PackageManager;
 import android.databinding.DataBindingUtil;
 import android.location.Location;
-import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.app.LoaderManager;
-import android.support.v4.content.ContextCompat;
 import android.support.v4.content.Loader;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -31,23 +22,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.appindexing.Thing;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.Result;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.common.api.Status;
-import com.google.android.gms.location.LocationListener;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.LocationSettingsRequest;
-import com.google.android.gms.location.LocationSettingsStatusCodes;
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
-import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
@@ -56,7 +31,7 @@ import com.sothree.slidinguppanel.SlidingUpPanelLayout;
 
 import java.util.List;
 
-import csfyp.cs_fyp_android.CustomFragment;
+import csfyp.cs_fyp_android.CustomMapFragment;
 import csfyp.cs_fyp_android.MainActivity;
 import csfyp.cs_fyp_android.R;
 import csfyp.cs_fyp_android.about.FrgAbout;
@@ -75,15 +50,7 @@ import csfyp.cs_fyp_android.setting.FrgSetting;
 import retrofit2.Call;
 import retrofit2.Response;
 
-import static android.app.Activity.RESULT_OK;
-
-public class FrgHome extends CustomFragment implements LoaderManager.LoaderCallbacks<List<Event>>,
-        GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener,
-        ResultCallback,
-        LocationListener,
-        OnMapReadyCallback,
-        GoogleMap.OnInfoWindowClickListener {
+public class FrgHome extends CustomMapFragment implements LoaderManager.LoaderCallbacks<List<Event>> {
 
     public static final int HOME_LOADER_ID = 1;
     public static final int HOME_LOCATION_SETTING_CALLBACK = 2;
@@ -93,11 +60,7 @@ public class FrgHome extends CustomFragment implements LoaderManager.LoaderCallb
     private boolean mIsPanelExpanded;
     private boolean mIsPanelAnchored;
     private boolean mIsLoadFinished = false;
-    private boolean mIsMapReady = false;
-    private boolean mIsPermissionGranted = false;
-    private boolean mIsLocationSettingEnable = false;
 
-    private boolean mIsSetToSelfLocation = false;
     private List<Event> mData;
 
     private HomeFrgBinding mDataBinding;
@@ -115,25 +78,13 @@ public class FrgHome extends CustomFragment implements LoaderManager.LoaderCallb
     // For Left Drawer
     private DrawerLayout mDrawerLayout;
 
-    // For Google Map
-    private LatLng mLastTarget;
-    private float mLastZoom;
-    private Location mCurrentLocation;
-    private LocationRequest mLocationRequest;
-    private LocationSettingsRequest mLocationSettingsRequest;
-    private MapView mMapView;
-    private GoogleMap mGoogleMap;
-    private GoogleApiClient client;
-
     // For Sliding Up Panel
     private SlidingUpPanelLayout mLayout;
 
     // For HTTP
     private Response<EventListRespond> mEventRespond;
 
-
-    public FrgHome() {
-    }
+    public FrgHome() {}
 
     public static FrgHome newInstance() {
 
@@ -157,52 +108,14 @@ public class FrgHome extends CustomFragment implements LoaderManager.LoaderCallb
         }
     }
 
-    private void buildGoogleApiClient() {
-        client = new GoogleApiClient.Builder(getContext())
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(LocationServices.API)
-                .addApi(AppIndex.API)
-                .build();
-    }
-
-    private void createLocationRequest() {
-        mLocationRequest = new LocationRequest();
-        mLocationRequest.setInterval(20000);
-        mLocationRequest.setFastestInterval(10000);
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
-    }
-
-    private void buildLocationSettingsRequest() {
-        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
-        builder.addLocationRequest(mLocationRequest);
-        builder.setAlwaysShow(true);
-        mLocationSettingsRequest = builder.build();
-    }
-
-    private void checkLocationSettings() {
-        LocationServices.SettingsApi.checkLocationSettings(client, mLocationSettingsRequest)
-                        .setResultCallback(this);
-    }
-
-    private void startLocationUpdate() {
-        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            if (mIsLocationSettingEnable && mIsPermissionGranted) {
-                LocationServices.FusedLocationApi.requestLocationUpdates(client, mLocationRequest, this);
-            }
-        }
-    }
-
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
+        isUseLocationService = true;
         super.onCreate(savedInstanceState);
 
         // initialize Pull-up Panel, try getting the last status
         if (savedInstanceState != null) {
             mIsPanelExpanded = savedInstanceState.getBoolean("isPanelExpanded");
-            mLastTarget = savedInstanceState.getParcelable("lastTarget");
-            mLastZoom = savedInstanceState.getFloat("lastZoom");
-            mIsSetToSelfLocation = savedInstanceState.getBoolean("isSetToSelfLocation");
         }
 
         setHasOptionsMenu(true);
@@ -212,20 +125,19 @@ public class FrgHome extends CustomFragment implements LoaderManager.LoaderCallb
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putParcelable("lastTarget", mLastTarget);
-        outState.putFloat("lastZoom", mLastZoom);
         outState.putBoolean("isPanelExpanded", mIsPanelExpanded);
-        outState.putBoolean("isSetToSelfLocation", mIsSetToSelfLocation);
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        super.onCreateView(inflater, container, savedInstanceState);
         mDataBinding = DataBindingUtil.inflate(inflater, R.layout.home_frg, container, false);
         mDataBinding.setHandlers(this);
         View v = mDataBinding.getRoot();
         MainActivity parentActivity = (MainActivity) getActivity();
+
+        mMapView = mDataBinding.homeMap;
+        super.onCreateView(inflater, container, savedInstanceState);
 
         // Setting up Action Bar
         mToolBar = mDataBinding.homeToolbar;
@@ -255,36 +167,12 @@ public class FrgHome extends CustomFragment implements LoaderManager.LoaderCallb
         // Setting up Navigation Drawer
         mDrawerLayout = mDataBinding.drawerLayout;
 
-        // Setting up Google Map
-        new Runnable() {
-            @Override
-            public void run() {
-                mMapView.onCreate(null);
-            }
-        };
-
-        mMapView = mDataBinding.homeMap;
-
-        if (savedInstanceState != null) {
-            Bundle mapState;
-            mapState = savedInstanceState.getBundle("mapSaveInstanceState"); // TODO: 19/10/2016 change key
-            mMapView.onCreate(mapState);
-        }
-        else
-            mMapView.onCreate(null);
-
-        mMapView.getMapAsync(this);
-
-
         // Setting up RcyclerView for event
         mEventRecyclerView = mDataBinding.rvEvent;
         mEventLayoutManager = new LinearLayoutManager(getContext());
         mEventRecyclerView.setLayoutManager(mEventLayoutManager);
         mEventAdapter = new AdtEvent(this);
         mEventRecyclerView.setAdapter(mEventAdapter);
-
-        // check location setting
-        buildGoogleApiClient();
 
         // set self user
         mDataBinding.homeUsername.setText(((MainActivity)getActivity()).getmUsername());
@@ -335,73 +223,13 @@ public class FrgHome extends CustomFragment implements LoaderManager.LoaderCallb
     }
 
     @Override
-    public void onStart() {
-        super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        AppIndex.AppIndexApi.start(client, getIndexApiAction());
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        AppIndex.AppIndexApi.end(client, getIndexApiAction());
-        client.disconnect();
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        if (mMapView != null)
-            mMapView.onResume();
-        if (client.isConnected()) {
-            startLocationUpdate();
-        }
-    }
-
-    @Override
-    public void onPause() {
-
-        // TODO: 22/11/2016 pause network 
-        super.onPause();
-        mMapView.onPause();
-        if (client.isConnected())
-            LocationServices.FusedLocationApi.removeLocationUpdates(client, this);
-    }
-
-    @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         inflater.inflate(R.menu.option_menu, menu);
     }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        mMapView.onDestroy();
-    }
-
-    @Override
-    public void onLowMemory() {
-        super.onLowMemory();
-        mMapView.onLowMemory();
-    }
-
-    @Override
+    @Override // TODO: 3/1/2017 override super here
     public void onMapReady(GoogleMap googleMap) {
-        mGoogleMap = googleMap;
-        mIsMapReady = true;
-        mGoogleMap.setOnInfoWindowClickListener(this);
-        mGoogleMap.getUiSettings().setZoomControlsEnabled(true);
-        mGoogleMap.getUiSettings().setMyLocationButtonEnabled(true);
-        mGoogleMap.getUiSettings().setMapToolbarEnabled(false);
-        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            mGoogleMap.setMyLocationEnabled(true);
-        }
+        super.onMapReady(googleMap);
 
         mGoogleMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
             @Override
@@ -430,93 +258,13 @@ public class FrgHome extends CustomFragment implements LoaderManager.LoaderCallb
             }
         });
 
-        if (mLastTarget != null)
-            mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mLastTarget, mLastZoom));
-
         populateMapMarker();
     }
 
-    @Override
+    @Override // TODO: 3/1/2017 override super here
     public void onLocationChanged(Location location) {
-        mCurrentLocation = location;
+        super.onLocationChanged(location);
         getLoaderManager().restartLoader(HOME_LOADER_ID, null, this);
-        if (!mIsSetToSelfLocation) {
-            mGoogleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude()), 11.0f));
-            mIsSetToSelfLocation = true;
-        }
-    }
-
-    @Override
-    public void onResult(@NonNull Result result) {
-        final Status status = result.getStatus();
-        switch (status.getStatusCode()) {
-            case LocationSettingsStatusCodes.SUCCESS:
-                mIsLocationSettingEnable = true;
-                startLocationUpdate();
-                break;
-            case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
-                try {
-                    status.startResolutionForResult(getActivity(), HOME_LOCATION_SETTING_CALLBACK);
-                } catch (IntentSender.SendIntentException e) {
-                    // TODO: 27/10/2016 ignore?
-                    Toast.makeText(getContext(),"Try get Location fail", Toast.LENGTH_LONG).show();
-                }
-                break;
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == HOME_PERMISSION_CALLBACK) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                mIsPermissionGranted = true;
-                startLocationUpdate();
-            }
-        }
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        // result for location setting prompt
-        if (requestCode == HOME_LOCATION_SETTING_CALLBACK) {
-            if (resultCode == RESULT_OK) {
-                mIsLocationSettingEnable = true;
-                startLocationUpdate();
-            } else {
-                LocationServices.SettingsApi.checkLocationSettings(client, mLocationSettingsRequest)
-                        .setResultCallback(this);
-            }
-        }
-    }
-
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-        // check location setting
-
-        createLocationRequest();
-        buildLocationSettingsRequest();
-        checkLocationSettings();
-
-        // check permission for location
-        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION)) {
-            } else {
-                ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, HOME_PERMISSION_CALLBACK);
-            }
-        } else {
-            // permission granted
-            mIsPermissionGranted = true;
-            startLocationUpdate();
-        }
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
     }
 
     @Override
@@ -544,7 +292,7 @@ public class FrgHome extends CustomFragment implements LoaderManager.LoaderCallb
                             return null;
                         }
                     } catch (Exception e) {
-                        Log.i(TAG, "Connect exception");
+                        Log.i(TAG, "Connect exception:" + e.getMessage());
                         return null;
                     }
                 } else
@@ -581,11 +329,10 @@ public class FrgHome extends CustomFragment implements LoaderManager.LoaderCallb
         }
     }
 
-    @Override
+    @Override // TODO: 3/1/2017 override super here
     public void onInfoWindowClick(Marker marker) {
+        super.onInfoWindowClick(marker);
         String[] temp = marker.getSnippet().split("&");
-        mLastTarget = mGoogleMap.getCameraPosition().target;
-        mLastZoom = mGoogleMap.getCameraPosition().zoom;
         switchFragment(FrgEvent.newInstance(Integer.parseInt(temp[4])));
     }
 
@@ -593,7 +340,7 @@ public class FrgHome extends CustomFragment implements LoaderManager.LoaderCallb
         mLastTarget = mGoogleMap.getCameraPosition().target;
         mLastZoom = mGoogleMap.getCameraPosition().zoom;
         super.switchFragment(this, fragment);
-    }
+    } // TODO: 3/1/2017 remove
 
     public void onClickNewEvent(View view) {
         if(mCurrentLocation != null)
@@ -615,26 +362,6 @@ public class FrgHome extends CustomFragment implements LoaderManager.LoaderCallb
     }
 
     public void onClickHistory(View view){ switchFragment(FrgHistory.newInstance());}
-
-    public Action getIndexApiAction() {
-        Thing object = new Thing.Builder()
-                .setName("Main Page") // TODO: Define a title for the content shown.
-                // TODO: Make sure this auto-generated URL is correct.
-                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
-                .build();
-        return new Action.Builder(Action.TYPE_VIEW)
-                .setObject(object)
-                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
-                .build();
-    }
-
-    class LoadingMapAsyncTask extends AsyncTask<String, Integer, Integer> {
-
-        @Override
-        protected Integer doInBackground(String... strings) {
-            return null;
-        }
-    }
 }
 
 
